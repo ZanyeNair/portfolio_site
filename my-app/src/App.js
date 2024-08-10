669e87f874cd7859d39e92e33c38d717f0a4f67b
ZanyeNair <141592436+ZanyeNair@users.noreply.github.com>
2024-08-10 00:11:00 -0400

    creation

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import Models from './models';
+import './App.css';
+
+
+function App() {
+  return (
+    
+    <div className="App">
+      <h1>My First React App</h1>
+    
+    <Models />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Models.js
@@ -0,0 +1,13 @@
+import * as THREE from 'three';
+import React from 'react';
+
+function Models() {
+    return (
+      <div>
+       <p>Models</p>
+      </div>
+    );
+  }
+  
+  export default Models;
+  
