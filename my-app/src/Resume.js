bb6c7600eeb2e074caf48ce12aec608190a031ec
ZanyeNair <141592436+ZanyeNair@users.noreply.github.com>
2024-09-29 21:58:38 -0400

    finish content

--- my-app/src/Resume.js
+++ my-app/src/Resume.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function Resume() {
   return (
-    <div>
-      <iframe src="https://www.google.com/" width="400" height="400"></iframe>
+    <Container>
+      <Row>
+        <Col>
+          <h1>Resume</h1>
+          <div id = "resume">
+      <embed src="resumezmn.pdf" width="800px" height="1100px" />
     </div>
+        </Col>
+        <Col>
+          <Container>
+            <Row>
+              <Col>
+              <button>
+                <a href="resumezmn.pdf" download>Download</a>
+              </button>
+              </Col>
+            </Row>
+            <Row>
+              <button>
+                <a href="https://github.com/ZanyeNair" target="_blank">Open Github</a>
+              </button>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+      
+    </Container>
+    
   )
 }
 
